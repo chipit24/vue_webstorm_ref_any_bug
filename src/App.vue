ba0dce0741f9fd134ecd6a1ca81da9ec59fc670f
Robert Komaromi <chipit24@users.noreply.github.com>
2022-11-18 12:27:44 -0500

    Add component template ref

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import {ref} from "vue";
+
+const helloWorld = ref<InstanceType<typeof HelloWorld> | null>(null);
 </script>
 
 <template>
@@ -8,7 +11,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld ref="helloWorld" msg="You did it!" />
     </div>
   </header>
 
